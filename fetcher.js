8da6d960cb97219cfd9882a7e05da196a1ec0502
Soren Nissen <s.nissen.bass@gmail.com>
2020-09-24 01:49:22 +0000

    added chalk npm colors to prompts and console logs

--- fetcher.js
+++ fetcher.js
@@ -2,6 +2,7 @@ const readline = require('readline')
 const request = require('request');
 const fs = require('fs');
 const {stdout} = require('process');
+const chalk = require('chalk')
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -10,31 +11,37 @@ const rl = readline.createInterface({
 const user = {};
 
 
-rl.question('Enter the URL:\n', (answer) => {
+//Ask user for inputs of URL and FileName in the terminal and save them in User Object.
+
+rl.question(chalk.green('Enter the URL:\n'), (answer) => {
   user.url = answer;
 
 
-  rl.question('Enter the file path: ex ./nameOfFile.txt\n', (answer) => {
+  rl.question(chalk.green('Enter the file path: ex ./nameOfFile.txt\n'), (answer) => {
     user.file = answer;
 
-    console.log(user);
+    //Use request to connect to the server and get the file info
     request(user.url, (error, response, body) => {
       if (error) {
         stdout.write('There has been an error with the URL that you provided. Please try again. \n')
         return error;
       }
+
+      //Store the file size of the request body for later console.log
       let size = response.headers['content-length'];
 
+      //write the file based on the the user object input above
       fs.writeFile(user.file, body, (err) => {
-        // throws an error, you could also catch it here
+
+        // throws the error if there is one
         if (err) throw err;
 
-        // success case, the file was saved
-        console.log(`Downloaded and saved ${size} bytes to ${user.file}`);
+        // success case, the file was saved. 
+        console.log(chalk.green('Downloaded and saved ' + chalk.red(size) + " bytes to " + chalk.red(user.file)));
       });
 
+
     });
-    // process.stdout.write(bio);
     rl.close();
   })
 });
